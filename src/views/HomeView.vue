5c9fed16fec1f0dd5ac303bbfc3c57dc1f6ba738
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-01 23:27:27 +0700

    Install Tailwind + Install and configure tailwind w/ project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="bg-slate-900">
+    <section>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt nemo eum, laudantium autem saepe sit expedita. Vero architecto blanditiis magnam porro quasi vel fuga, deleniti veritatis nesciunt provident nobis quo voluptates dolore sed necessitatibus fugit soluta cum sunt ratione, saepe modi officiis iure? Nemo ullam unde reprehenderit voluptatibus corporis tempore architecto, quod animi. In ipsam veniam perferendis deleniti voluptatum magni eaque ratione fugit a rerum, molestiae, vel ab culpa dolorum illo laboriosam ipsa laborum. Voluptatum, delectus iusto! Nisi fugiat asperiores tempore inventore provident consequatur magnam officia, dolorem voluptate officiis obcaecati, dignissimos quae amet dolore esse. Maxime repellat aliquid accusantium.</p>
+    </section>
   </main>
 </template>
