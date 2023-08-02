fecef89ff9957e0aeeea5c928b7f4ddf005da35f
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-02 17:19:20 +0700

    Create UniicodeAnchor.vue

---
+++ src/components/UniicodeAnchor.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+  to: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <a :href="to" class="button-uniicode">
+    <slot></slot>
+  </a>
+</template>
