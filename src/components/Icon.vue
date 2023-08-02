36dba0bf7e9252a121748fc592b0fc6cb0142f43
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-02 11:04:43 +0700

    Create Icon.vue

---
+++ src/components/Icon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+  icon: {
+    type: String,
+    default: 'favorite'
+  }
+})
+</script>
+
+<template>
+  <span class="material-symbols-sharp">
+    {{ icon }}
+  </span>
+</template>
