0fc3d3b30e0326b4d084a805bddffd2203f285d5
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-02 17:21:01 +0700

    Create UniicodeRouterLink.vue

---
+++ src/components/UniicodeRouterLink.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+defineProps({
+  link: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <RouterLink :to="link">
+    <slot></slot>
+  </RouterLink>
+</template>
