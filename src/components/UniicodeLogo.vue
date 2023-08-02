3da907137e367497973d10beca862dd5bfcb6462
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-02 12:32:49 +0700

    Create UniicodeLogo.vue

---
+++ src/components/UniicodeLogo.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import uniicodeJpg from '../assets/images/uniicode.jpg'
+import uniicodeWebp from '../assets/images/uniicode.webp'
+
+// Check WebP support
+const supportsWebp = (): boolean => {
+  const canvas = document.createElement('canvas');
+  return canvas.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+}
+
+const isWebpSupported = supportsWebp();
+</script>
+
+<template>
+  <picture class="uniicode-logo-container">
+    <source
+      v-if="isWebpSupported"
+      :srcset="isWebpSupported ? uniicodeWebp : uniicodeJpg"
+      type="image/webp"
+    />
+    <img
+      :src="uniicodeJpg"
+      alt="UNIICODE logo"
+      class="uniicode-logo"
+    />
+  </picture>
+</template>
+
+<style scoped>
+.uniicode-logo-container {
+  max-width: 100%;
+}
+
+.uniicode-logo {
+  margin: 5px;
+  width: 100px;
+  border-radius: 50%;
+  animation: live 5s infinite alternate forwards;
+}
+
+
+@keyframes live {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1.085);
+  }
+}
+</style>
