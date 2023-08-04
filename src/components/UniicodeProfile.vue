0b2a9b1ab1f0e59e899b4af7ae33687fdfe26d74
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-04 23:16:54 +0700

    Integrate UniicodeProfile styles - Apply `profile-uniicode` styles

---
+++ src/components/UniicodeProfile.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: false
+  }
+})
+
+const profileDetailsUniicode = ref<HTMLElement|null>(null)
+
+const slideInProfileDetails = () => {
+  const elem = profileDetailsUniicode.value
+
+  elem?.classList.add('profile-slide-left')
+}
+
+const slideOutProfileDetails = () => {
+  const elem = profileDetailsUniicode.value
+
+  elem?.classList.remove('profile-slide-left')
+}
+
+defineExpose({
+  slideInProfileDetails
+})
+
+</script>
+
+<template>
+  <article class="profile-uniicode">
+    <section
+      class="profile-image-uniicode"
+      @mouseover="slideInProfileDetails"
+      @mouseleave="slideOutProfileDetails"
+    >
+      <img
+        :src="image"
+        :alt="`${name} image`"
+        @mouseover="slideInProfileDetails"
+        @mouseleave="slideOutProfileDetails"
+      />
+    </section>
+    <section class="profile-details-uniicode" ref="profileDetailsUniicode">
+      {{ name }}
+    </section>
+  </article>
+</template>
