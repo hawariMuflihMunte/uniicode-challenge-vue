097a82d797d84f599706b662fb11e1d30f7019b0
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-06 22:02:39 +0700

    Create UniicodeFooter component

---
+++ src/components/UniicodeFooter.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <footer class="flex gap-9 justify-center py-2 px-4">
+    <span>ユニーコード</span>
+    <span>ゆにこおど</span>
+    <span class="center">UNIICODE</span>
+    <span>UNÜCODE</span>
+    <span>UÑÜÇÔDĒ</span>
+  </footer>
+</template>
+
+<style scoped>
+footer span {
+  transition: all 250ms linear;
+}
+
+footer span.center {
+  font-weight: 600;
+  font-size: larger;
+}
+
+footer span:hover {
+  font-size: large;
+  filter: drop-shadow(0 1px 12px #B8FA00);
+}
+
+footer span.center:hover {
+  font-size: x-large;
+}
+
+footer *::selection {
+  @apply text-uniicode-dark;
+
+  background: #B8FA00;
+  border-radius: 8px;
+}
+</style>
