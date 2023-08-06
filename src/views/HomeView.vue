d93180bd0be7a5378f396cd8ab64bf7b6c548440
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-08-06 14:14:55 +0700

    Insert UNIICODE Members - Galih Adhi Kusuma - Hawari Muflih Munte - Amir Zuhdi Wibowo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,81 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import UniicodeProfile from '@/components/UniicodeProfile.vue';
-import galih from '@/assets/images/person/galih.jpg'
+import UniicodeProfile from '@/components/UniicodeProfile.vue'
+import galihImg from '@/assets/images/person/galih.jpg'
+import hawariImg from '@/assets/images/person/hawari.jpg'
+import cakAdiImg from '@/assets/images/person/cakAdi.jpg'
+
+const personStats = [
+  {
+    name: 'Galih Adhi Kusuma',
+    image: galihImg,
+    strength: [
+      'UNIICODE Adomin'
+    ],
+    weakness: [
+      'For now, none'
+    ],
+    techStack: [
+      'javascript',
+      'react',
+      'express',
+      'cpp',
+      'go'
+    ]
+  },
+  {
+    name: 'Hawari Muflih Munte',
+    image: hawariImg,
+    strength: [
+      'UNIICODE Member',
+      'GitHub Contrast'
+    ],
+    weakness: [
+      'Noob'
+    ],
+    techStack: [
+      'html',
+      'css',
+      'javascript',
+      'typescript',
+      'ruby',
+      'cpp',
+      'vue',
+      'react'
+    ]
+  },
+  {
+    name: 'Amir Zuhdi Wibowo',
+    image: cakAdiImg,
+    strength: [
+      'UNIICODE Sepoeh 🙏🏼',
+      '!mockable_php'
+    ],
+    weakness: [
+      'None, sepoerane 🙏🏼'
+    ],
+    techStack: [
+      'javascript',
+      'react',
+      'express'
+    ]
+  },
+]
+
 </script>
 
 <template>
   <section>
     <h1 class="text-4xl text-center text-slate-300">ユニーコード</h1>
     <section class="container py-10">
-      <UniicodeProfile
-        name="Galih Adhi Kusuma"
-        :image="galih"
-        :strength="[
-          'UNIICODE Adomin'
-        ]"
-        :weakness="[
-          'For now, none'
-        ]"
-        :tech-stack="[
-          'javascript',
-          'react',
-          'express'
-        ]"
-      />
+      <template v-for="person in personStats" :key="person">
+        <UniicodeProfile
+          :name="person.name"
+          :image="person.image"
+          :strength="person.strength"
+          :weakness="person.weakness"
+          :tech-stack="person.techStack"
+        />
+      </template>
     </section>
   </section>
 </template>
